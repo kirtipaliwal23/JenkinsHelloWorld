eb1c5291cd01da584994f3b2b07e33e701e1b6a5
kirtipaliwal23 <68902768+kirtipaliwal23@users.noreply.github.com>
2020-09-07 15:07:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
  public static void main(String[] args){
-   System.out.println("Welcome to Jenkins training");
+   System.out.println("Welcome to Jenkins training,here am making changes");
 }
 }
